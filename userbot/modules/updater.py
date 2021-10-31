a890a9b76f5fe95ee46c0a9f89b4816762fc1411
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-11-01 02:56:46 +0700

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -53,7 +53,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
         if HEROKU_APP_NAME is None:
             await event.edit(
                 '`[HEROKU]: Harap Siapkan Variabel` **HEROKU_APP_NAME** `'
-                ' untuk dapat deploy perubahan terbaru dari 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓`'
+                ' untuk dapat deploy perubahan terbaru dari` **Zeinzo-Userbot**'
             )
             repo.__del__()
             return
@@ -91,7 +91,7 @@ async def deploy(event, repo, ups_rem, ac_br, txt):
             await asyncio.sleep(5)
             return await event.delete()
         else:
-            await event.edit("`KEN-UBOT Berhasil Di Deploy!\n" "Restarting, Mohon Tunggu Sebentar.....`")
+            await event.edit("`Zeinzo-Userbot Berhasil Di Deploy!\n" "Restarting, Mohon Tunggu Sebentar.....`")
             await asyncio.sleep(15)
             await event.delete()
 
@@ -115,9 +115,9 @@ async def update(event, repo, ups_rem, ac_br):
     except GitCommandError:
         repo.git.reset("--hard", "FETCH_HEAD")
     await update_requirements()
-    await event.edit('**𝐊𝐄𝐍-𝐔𝐁𝐎𝐓** `Berhasil Di Update!`')
+    await event.edit('**Zeinzo-Userbot** `Berhasil Di Update!`')
     await asyncio.sleep(1)
-    await event.edit('**𝐊𝐄𝐍-𝐔𝐁𝐎𝐓** `Di Restart....`')
+    await event.edit('**Zeinzo-Userbot** `Di Restart....`')
     await asyncio.sleep(1)
     await event.edit('`Mohon Menunggu Beberapa Detik.`')
     await asyncio.sleep(10)
@@ -126,7 +126,7 @@ async def update(event, repo, ups_rem, ac_br):
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID, "#BOT \n"
-            "**𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 Telah Di Perbarui.**")
+            "**Zeinzo-Userbot Telah Di Perbarui.**")
         await asyncio.sleep(100)
         await event.delete()
 
@@ -188,13 +188,13 @@ async def upstream(event):
 
     if changelog == '' and force_update is False:
         await event.edit(
-            f'\n☄️ 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 Sudah Versi Terbaru\n')
+            f'\n☄️ Zeinzo-Userbot Sudah Versi Terbaru\n')
         await asyncio.sleep(15)
         await event.delete()
         return repo.__del__()
 
     if conf is None and force_update is False:
-        changelog_str = f'**Pembaruan Untuk 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓 :\n\n➣ Pembaruan Data :**\n`{changelog}`'
+        changelog_str = f'**Pembaruan Untuk Zeinzo-Userbot :\n\n➣ Pembaruan Data :**\n`{changelog}`'
         if len(changelog_str) > 4096:
             await event.edit("`Changelog Terlalu Besar, Lihat File Untuk Melihatnya.`")
             file = open("output.txt", "w+")
@@ -214,12 +214,12 @@ async def upstream(event):
         await event.edit(
             '`Sinkronisasi Paksa Ke Kode Userbot Stabil Terbaru, Harap Tunggu .....`')
     else:
-        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....1%`')
-        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....20%`')
-        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....35%`')
-        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Loading....77%`')
-        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Updating...90%`')
-        await event.edit('` Proses Update 𝐊𝐄𝐍-𝐔𝐁𝐎𝐓, Mohon Tunggu Sebentar....100%`')
+        await event.edit('` Proses Update Zeinzo-Userbot, Loading....1%`')
+        await event.edit('` Proses Update Zeinzo-Userbot, Loading....20%`')
+        await event.edit('` Proses Update Zeinzo-Userbot, Loading....35%`')
+        await event.edit('` Proses Update Zeinzo-Userbot, Loading....77%`')
+        await event.edit('` Proses Update Zeinzo-Userbot, Updating...90%`')
+        await event.edit('` Proses Update Zeinzo-Userbot, Mohon Tunggu Sebentar....100%`')
     if conf == "now":
         await update(event, repo, ups_rem, ac_br)
         await asyncio.sleep(10)
