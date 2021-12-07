9b11f2735aa181a6d042019aacaae4605a588927
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-07 16:27:15 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -310,14 +310,14 @@ async def redis(alive):
     await asyncio.sleep(3)
     output = (
         f"**[Seth-Ubot](https://github.com/Dorimuhai/Seth-Userbot) Has Started !**\n"\n"
-        f" ⚡**Master :**`{DEFAULTUSER} \n"
-        f" ⚡**Username :**`@{user.username} \n"
-        f" ⚡**Telethon :**`Ver {version.__version__} \n"
-        f" ⚡**Python :**`Ver {python_version()} \n"
-        f" ⚡**Branch :**`{UPSTREAM_REPO_BRANCH} \n"
-        f" ⚡**Bot Ver :**`{BOT_VER} \n"
-        f" ⚡**Modules :**`{len(modules)} Modules \n"
-        f"            **[Support](https://t.me/SeiraSupport)**\n")
+        f" `⚡**Master :** `{DEFAULTUSER} \n"
+        f" `⚡**Username :** `@{user.username} \n"
+        f" `⚡**Telethon :** `Ver {version.__version__} \n"
+        f" `⚡**Python :** `Ver {python_version()} \n"
+        f" `⚡**Branch :** `{UPSTREAM_REPO_BRANCH} \n"
+        f" `⚡**Bot Ver :** `{BOT_VER} \n"
+        f" `⚡**Modules :** `{len(modules)} Modules \n"
+        f"            **[Support](https://t.me/SeiraSupport)**")
     if ALIVE_LOGO:
         try:
             logo = ALIVE_LOGO
